5f0af9d795e04d5951022c9b728201a8c244c282
mohannadabdo <mohannad.abdo@uoit.net>
2017-10-24 14:25:49 -0400

    First Commit

---
+++ Assignment1/app/src/main/java/com/example/a100523158/assignment1/AskQuestion.java
@@ -0,0 +1,17 @@
+package com.example.a100523158.assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AskQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_question);
+
+
+    }
+
+
+}
